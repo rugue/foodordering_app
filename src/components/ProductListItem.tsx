1b7645496bea776e36a9f44055e18f9c0f272d06
Osarugue Enehizena <nehirugue@gmail.com>
2024-06-07 00:01:57 +0100

    FlatList done

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -17,6 +17,7 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
       <Image
         source={{ uri: product.image || defaultPizzaImage }}
         style={styles.image}
+        resizeMode="contain"
       />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
@@ -30,7 +31,9 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "white",
     padding: 10,
-    borderRadius: 10,
+    borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
   },
   title: {
     fontSize: 18,
