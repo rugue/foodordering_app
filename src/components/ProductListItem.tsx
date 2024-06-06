ef2eadcbc9698781d2bf87e25cb3dce737cb984a
Osarugue Enehizena <nehirugue@gmail.com>
2024-06-06 21:56:15 +0100

    first custom component

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,41 @@
+//27.44 of video in vlc
+import { StyleSheet, Text, View, Image } from "react-native";
+import Colors from "@/src/constants/Colors";
+import products from "../../assets/data/products";
+
+const product = products[1];
+
+const ProductListItem = ({ product }) => {
+  // console.log(props);
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+});
