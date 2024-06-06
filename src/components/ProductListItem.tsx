cb14eab8645b45444a76fe98b717a4fef6142688
Osarugue Enehizena <nehirugue@gmail.com>
2024-06-06 22:41:04 +0100

    Type sage components

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,15 +1,23 @@
 //27.44 of video in vlc
 import { StyleSheet, Text, View, Image } from "react-native";
-import Colors from "@/src/constants/Colors";
-import products from "../../assets/data/products";
+import Colors from "../constants/Colors";
+import { Product } from "../types";
 
-const product = products[1];
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
 
-const ProductListItem = ({ product }) => {
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
   // console.log(props);
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
