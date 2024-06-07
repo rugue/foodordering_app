8a470c827549e8c1a38d7278563953f86ad6f81f
Osarugue Enehizena <nehirugue@gmail.com>
2024-06-07 13:47:30 +0100

    working on details screen

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,15 +1,45 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import { Stack, useLocalSearchParams } from "expo-router";
+import products from "@assets/data/products";
+import { defaultPizzaImage } from "@/components/ProductListItem";
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
 
+  const product = products.find((p) => p.id.toString() === id);
+
+  if (!product) {
+    return <Text>Product not found</Text>;
+  }
+
   return (
-    <View>
-      <Stack.Screen options={{ title: "Details: " + id }} />
-      <Text style={{ fontSize: 20 }}>ProductDetailsScreen for id: {id}</Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 };
 
 export default ProductDetailsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+
+  price: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
