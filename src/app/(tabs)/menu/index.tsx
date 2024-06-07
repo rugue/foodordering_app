738240ee0ce3095d0cd3de93623ae5c0dad935ef
Osarugue Enehizena <nehirugue@gmail.com>
2024-06-07 13:03:21 +0100

    still on navigation

---
+++ src/app/(tabs)/menu/index.tsx
@@ -0,0 +1,16 @@
+//27.44 of video in vlc
+import { FlatList } from "react-native";
+import products from "@assets/data/products";
+import ProductListItem from "@components/ProductListItem";
+
+export default function MenuScreen() {
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+      numColumns={2}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      columnWrapperStyle={{ gap: 10 }}
+    />
+  );
+}
