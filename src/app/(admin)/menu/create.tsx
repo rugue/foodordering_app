42d23bf6bb359baaafb180082329f0df70651beb
Osarugue Enehizena <nehirugue@gmail.com>
2024-06-09 20:32:04 +0100

    basic input validation

---
+++ src/app/(admin)/menu/create.tsx
@@ -0,0 +1,90 @@
+import Button from "@/components/Button";
+import { parse } from "@babel/core";
+import { useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+
+const CreateProductScreen = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const [errors, setErrors] = useState("");
+
+  const resetFields = () => {
+    setName("");
+    setPrice("");
+  };
+
+  const validateInput = () => {
+    setErrors("");
+    if (!name) {
+      setErrors("Name is required");
+      return false;
+    }
+    if (!price) {
+      setErrors("Price is required");
+      return false;
+    }
+
+    if (isNaN(parseFloat(price))) {
+      setErrors("Price must be a number");
+      return false;
+    }
+
+    return true;
+  };
+
+  const onCreate = () => {
+    if (!validateInput()) return;
+
+    console.warn("Creating Product: ", name, price);
+    //Save in the database
+
+    resetFields();
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Name</Text>
+      <TextInput
+        value={name}
+        onChangeText={setName}
+        placeholder="Name"
+        style={styles.input}
+      />
+
+      <Text style={styles.label}>Price ($)</Text>
+      <TextInput
+        value={price}
+        onChangeText={setPrice}
+        placeholder="9.99"
+        style={styles.input}
+        keyboardType="numeric"
+      />
+
+      <Text style={{ color: "red" }}>{errors}</Text>
+      <Button onPress={onCreate} text="Create" />
+    </View>
+  );
+};
+
+export default CreateProductScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 10,
+  },
+
+  input: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 5,
+    marginBottom: 20,
+  },
+
+  label: {
+    color: "gray",
+    fontSize: 16,
+  },
+});
